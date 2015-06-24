e8fe58af0a761410702f50eaf455c3b5c626b7d3
Chad <chadcodes@users.noreply.github.com>
2015-06-24 18:00:37 -0400

    setup views on main storyboard controller

--- FoodAndFriends/SignUpController.swift
+++ FoodAndFriends/SignUpController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignUpController.swift
 //  FoodAndFriends
 //
 //  Created by bitmaker on 2015-06-24.
@@ -8,23 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignUpController: UIViewController {
 
-    @IBAction func SignUp(sender: AnyObject) {
-        
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
-
